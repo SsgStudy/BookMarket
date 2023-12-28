42c911f1a67f50ed73d4f321a0892d6ed69db2cc
장서윤 <seoy316@naver.com>
2023-12-28 17:47:09 +0900

    create: mapper interface 생성

--- BookCart/src/mapper/BookMapper.java
+++ BookCart/src/mapper/BookMapper.java
@@ -1,63 +1,24 @@
 package mapper;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import vo.Book;
+import entity.Book;
 import vo.CartElement;
 
-public class BookMapper {
-    private Map<String, Book> books = new HashMap<>();
-    private Map<String, List<CartElement>> cart = new HashMap<>();
+import java.util.List;
+import java.util.Map;
 
-    public BookMapper() {
-        books.put("ISBN1234", new Book("ISBN1234", "쉽게 배우는 JSP 웹 프로그래밍", 27000, "송미영", "단계별로 쇼핑몰을 구현하며 배우는 JSP 웹 프로그래밍", "IT전문서", "2018/10/08"));
-        books.put("ISBN1235", new Book("ISBN1235", "안드로이드 프로그래밍", 33000, "우재남", "실습 단계별 명쾌한 멘토링!", "IT전문서", "2022/01/22"));
-        books.put("ISBN1236", new Book("ISBN1236", "스크래치", 22000, "고광일", "컴퓨팅 사고력을 키우는 블록 코딩", "컴퓨터 입문", "2019/06/10"));
-    }
+public interface BookMapper {
 
-    // 장바구니에 도서 담기
-    public void save(String userId, String bookId) {
-        List<CartElement> userCart = cart.get(userId);
-        boolean flag = false;
+    // C
+    void save(String userId, CartElement element);
 
-        if (userCart == null) {
-            userCart = new ArrayList<>();
-            cart.put(userId, userCart);
-        }
-        for (int i = 0; i < userCart.size(); i++) {
-            if(userCart.get(i).getBook().getBookId().equals(bookId))
-            {
-                flag = true;
-                cart.get(userId).get(i).plusAmount();
-                break;
-            }
-        }
-        if(flag == false)
-        {
-            cart.get(userId).add(new CartElement(books.get(bookId)));
-        }
-    }
+    // R
+    List<Book> findAll();
+    Book findByBookId(String bookId);
+    Map<String, Integer> findCartByUserId(String userId);
 
-    // 모든 도서 조회
-    public List<Book> findAll() {
-        return new ArrayList<>(books.values());
-    }
+    // U
 
-    // bookId로 Book 객체 찾아서 반환
-    public Book findByBookId(String bookId) {
-        return books.get(bookId);
-    }
+    // D
+    void clear(String userId);
 
-    // userId로 User 장바구니 조회
-    public List<CartElement> findCartByUserId(String userId) {
-        return cart.get(userId);
-    }
-
-    public void clear(String userId)
-    {
-        List<CartElement> userCart = cart.get(userId);
-        userCart.clear();
-    }
 }
