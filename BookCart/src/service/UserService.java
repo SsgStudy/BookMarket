ec0c4968d39ca2edb865ece377b95149ad4041e8
장서윤 <seoy316@naver.com>
2023-12-28 17:52:03 +0900

    create: 회원가입 기능 추가

--- BookCart/src/service/UserService.java
+++ BookCart/src/service/UserService.java
@@ -1,19 +1,43 @@
 package service;
 
-import mapper.PersonMapper;
-import vo.Person;
+import entity.Person;
+import mapper.PersonMapperImpl;
+
+import java.util.Scanner;
 
 public class UserService{
-    private PersonMapper personMapper = new PersonMapper();
+    private PersonMapperImpl personMapper = new PersonMapperImpl();
+    private BookService bookService = new BookService();
+
+    static Scanner sc = new Scanner(System.in);
 
     // 회원가입 (성공시 1 반환)
-    public int join(Person person) {
-        if (checkPersonInfo(person)) {
-            personMapper.save(person);
-            return 1;
+    public String join() {
+        Person user = inputSignInForm();
+
+        if (checkPersonInfo(user)) {
+            personMapper.save(user);
+            bookService.createUserBookCart(user.getId());
+            System.out.println("회원가입 성공");
+            return user.getId();
         } else {
-            return 0;
+            System.out.println("회원가입 실패");
+            return null;
         }
+    }
+
+    private Person inputSignInForm() {
+        //고객 정보 입력
+        System.out.print("당신의 이름을 입력하세요 : ");
+        String name = sc.nextLine();
+        System.out.print("연락처를 입력하세요 : ");
+        String phoneNumber = sc.nextLine();
+        System.out.print("아이디를 입력하세요 : ");
+        String id = sc.nextLine();
+        System.out.print("비밀번호를 입력하세요 : ");
+        String password = sc.nextLine();
+
+        return new Person.Builder(id, password, name, phoneNumber).build();
     }
 
     // 사용자 정보 유효성 검사
@@ -28,8 +52,11 @@ public class UserService{
     }
 
     // 사용자 정보 출력
-    public void menuGuestInfo(Person person) {
+    public void menuGuestInfo(String userId) {
+        Person person = personMapper.findById(userId);
         System.out.println("현재 고객 정보 : ");
-        System.out.println("이름 " + person.getName() + "연락처 " + person.getPhoneNumber());
+        System.out.println("이름 " + person.getName() +
+                            " 연락처 " + person.getPhoneNumber() +
+                            " 아이디 " + person.getId());
     }
 }
